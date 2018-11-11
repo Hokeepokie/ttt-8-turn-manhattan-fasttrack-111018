52a6b7159e1320a46dbadda720bcdb87185735fa
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 17:08:20 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,14 @@
+def display_board (Array)
+ #prints out board line by line
+ 
+end
+
+def valid_move? (display_board) 
+  #user input is the index
+  #returns true if index is 0-9 and unoccupied by a token
+  
+end
+
+def move (display_board)
+  #accepts index from user
+end
