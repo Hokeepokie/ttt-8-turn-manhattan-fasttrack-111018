49d343d3de9e7192850e3d09e921634f73c548ca
Eliza Cheung <elizacheung7@gmail.com>
2018-11-11 17:37:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,28 @@
 def display_board (Array)
- #prints out board line by line
- 
+ puts "_|_|_"
+ puts "_|_|_"
+ puts " | | "
 end
 
 def valid_move? (display_board) 
   #user input is the index
   #returns true if index is 0-9 and unoccupied by a token
-  
 end
 
 def move (display_board)
   #accepts index from user
-
 end
 
+
+
 def turn 
+ask for input
+get input
+convert input to index
+if index is valid
+  make the move for index
+  show the board
+else
+  ask for input again until you get a valid input
 end
 
